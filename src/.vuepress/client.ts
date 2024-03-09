a337a0dfc385742775c9dafea3b2b9aab023c3aa
cruld <cruldra@gmail.com>
2024-03-09 18:57:32 +0800

    react错误边界

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,25 +1,34 @@
-import { defineClientConfig } from "vuepress/client";
+import {defineClientConfig} from "vuepress/client";
 import 'uno.css';
 import CommandBuilder from "./components/CommandBuilder.vue";
 import ImageRenderer from "./components/ImageRenderer.vue";
-import  AudioPlayer from 'vue3-audio-player'
+import AudioPlayer from 'vue3-audio-player'
 import 'vue3-audio-player/dist/style.css'
-import { defineSandpackConfig } from "vuepress-plugin-md-enhance/client";
+import {defineSandpackConfig} from "vuepress-plugin-md-enhance/client";
 // import Test from "./components/Test.vue";
 // import AudioPlayer from 'vue3-audio-player'
 // import 'vue3-audio-player/dist/style.css'
 // import dayjs from "dayjs";
 export default defineClientConfig({
-    enhance: ({ app, router, siteData }) => {
+    enhance: ({app, router, siteData}) => {
         // alert(dayjs)
         // app.use(naive)
-       // app.component("Test", Test);
-       app.component("AudioPlayer", AudioPlayer);
-       app.component("CommandBuilder", CommandBuilder);
-       app.component("ImageRenderer", ImageRenderer);
-       //  app.use(RedisVersionHistories);
+        // app.component("Test", Test);
+        app.component("AudioPlayer", AudioPlayer);
+        app.component("CommandBuilder", CommandBuilder);
+        app.component("ImageRenderer", ImageRenderer);
+        //  app.use(RedisVersionHistories);
     },
 });
 defineSandpackConfig({
     // 这里是 sandpack 配置
+    options: {
+        //showInlineErrors: false
+    },
+    customSetup: {
+        dependencies: {
+            "react": "^18",
+            "react-dom": "^18",
+        }
+    }
 });
